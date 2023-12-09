7bb9bcd60f28a8807943881952dcdc560772d064
Felix <felix@felixleitner.com>
2023-12-09 21:59:49 +0000

    initial add_literal method

--- antwort-solver/src/ast.rs
+++ antwort-solver/src/ast.rs
@@ -1,9 +1,9 @@
 use crate::error::{Error, Result};
 
 pub struct Clause {
-    pos_literals: Vec<u64>,
-    neg_literals: Vec<u64>,
-    size: usize,
+    pos_literals: Vec<u64>, // Bit arrays of positive literals
+    neg_literals: Vec<u64>, // Each "slice" can hold 64 literals
+    size: i32,              // The number of literals in this clause
 }
 
 impl Clause {
@@ -15,12 +15,28 @@ impl Clause {
         }
     }
 
-    pub fn size(&self) -> usize {
+    /// The number of literals in this clause.
+    pub fn size(&self) -> i32 {
         self.size
     }
 
+    /// Adds a literal to this clause.
     pub fn add_literal(&mut self, literal: i32) -> Result<()> {
-        Err(Error::InvalidLiteral)
+        let dest = match literal.signum() {
+            1 => &mut self.pos_literals,
+            -1 => &mut self.neg_literals,
+            _ => return Err(Error::InvalidLiteral),
+        };
+        let literal = literal.abs() - 1; // From 1- to 0-indexed
+
+        let i = literal / 64;
+        let j = literal % 64;
+        if i as usize >= dest.len() {
+            // Grow the vector if necessary
+            dest.resize((i + 1) as usize, 0);
+        }
+        dest[i as usize] |= 1 << j;
+        Ok(()) // TODO: increase size
     }
 
     pub fn contains_literal(&self, literal: i32) -> bool {
