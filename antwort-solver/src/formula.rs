da1ffcbcc0314e2bae7040c2d7c822c6fd478bb6
Felix <felix@felixleitner.com>
2023-12-11 00:56:49 +0000

    some methods and tests

--- antwort-solver/src/formula.rs
+++ antwort-solver/src/formula.rs
@@ -3,3 +3,19 @@ use crate::clause::Clause;
 pub struct Formula {
     clauses: Vec<Clause>,
 }
+
+impl Formula {
+    pub fn new() -> Formula {
+        Formula {
+            clauses: Vec::new(),
+        }
+    }
+
+    pub fn add_clause(&mut self, clause: Clause) {
+        self.clauses.push(clause);
+    }
+
+    pub fn size(&self) -> usize {
+        self.clauses.len()
+    }
+}
---
+++ antwort-solver/tests/formula.rs
@@ -0,0 +1,25 @@
+use antwort_solver::{clause::Clause, error::Error, Formula};
+
+#[test]
+fn formula_new() {
+    let f = Formula::new();
+    assert_eq!(f.size(), 0);
+}
+
+#[test]
+fn formula_add_clause() {
+    let mut f = Formula::new();
+    let c = Clause::new();
+    f.add_clause(c);
+    assert_eq!(f.size(), 1);
+}
+
+#[test]
+fn formula_add_clauses() {
+    let mut f = Formula::new();
+    let c1 = Clause::new();
+    let c2 = Clause::new();
+    f.add_clause(c1);
+    f.add_clause(c2);
+    assert_eq!(f.size(), 2);
+}
