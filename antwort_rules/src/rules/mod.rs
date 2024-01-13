685e2634c854a4e50c90b5cf98e83e60eca752d1
Felix <felix@felixleitner.com>
2024-01-12 21:28:06 -0500

    implication, double-negation, flattening rules

--- antwort_rules/src/rules/mod.rs
+++ antwort_rules/src/rules/mod.rs
@@ -3,12 +3,12 @@ use antwort_core::{ast::Expr, rule::RuleApplicationError};
 use antwort_macros::register_rule;
 
 #[register_rule]
-pub fn example_rule(_expr: &Expr) -> Result<Expr, RuleApplicationError> {
+fn example_rule(_expr: &Expr) -> Result<Expr, RuleApplicationError> {
     Err(RuleApplicationError::RuleNotApplicable)
 }
 
 #[register_rule]
-pub fn de_morgans1(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+fn de_morgans1(expr: &Expr) -> Result<Expr, RuleApplicationError> {
     if let Expr::Negation(b) = expr {
         if let Expr::Disjunction(exprs) = b.as_ref() {
             return Ok(Expr::Conjunction(
@@ -23,7 +23,7 @@ pub fn de_morgans1(expr: &Expr) -> Result<Expr, RuleApplicationError> {
 }
 
 #[register_rule]
-pub fn de_morgans2(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+fn de_morgans2(expr: &Expr) -> Result<Expr, RuleApplicationError> {
     if let Expr::Negation(b) = expr {
         if let Expr::Conjunction(exprs) = b.as_ref() {
             return Ok(Expr::Disjunction(
@@ -33,6 +33,65 @@ pub fn de_morgans2(expr: &Expr) -> Result<Expr, RuleApplicationError> {
                     .collect(),
             ));
         }
+    }
+    Err(RuleApplicationError::RuleNotApplicable)
+}
+
+#[register_rule]
+fn double_negation(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    if let Expr::Negation(b) = expr {
+        if let Expr::Negation(expr) = b.as_ref() {
+            return Ok(expr.as_ref().clone());
+        }
+    }
+    Err(RuleApplicationError::RuleNotApplicable)
+}
+
+#[register_rule]
+fn implication_elimination(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    if let Expr::Implication(a, b) = expr {
+        return Ok(Expr::Disjunction(vec![
+            Expr::Negation(Box::new(a.as_ref().clone())),
+            b.as_ref().clone(),
+        ]));
+    }
+    Err(RuleApplicationError::RuleNotApplicable)
+}
+
+#[register_rule]
+fn flatten_conjunctions(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    if let Expr::Conjunction(exprs) = expr {
+        if exprs.iter().all(|e| !e.is_conjunction()) {
+            return Err(RuleApplicationError::RuleNotApplicable);
+        }
+        let mut new_exprs = vec![];
+        for e in exprs {
+            if let Expr::Conjunction(exprs) = e {
+                new_exprs.extend(exprs.iter().cloned());
+            } else {
+                new_exprs.push(e.clone());
+            }
+        }
+        return Ok(Expr::Conjunction(new_exprs));
+    }
+    Err(RuleApplicationError::RuleNotApplicable)
+}
+
+#[register_rule]
+fn flatten_disjunctions(expr: &Expr) -> Result<Expr, RuleApplicationError> {
+    if let Expr::Disjunction(exprs) = expr {
+        if exprs.iter().all(|e| !e.is_disjunction()) {
+            return Err(RuleApplicationError::RuleNotApplicable);
+        }
+        let mut new_exprs = vec![];
+        for e in exprs {
+            if let Expr::Disjunction(exprs) = e {
+                new_exprs.extend(exprs.iter().cloned());
+            } else {
+                new_exprs.push(e.clone());
+            }
+        }
+        return Ok(Expr::Disjunction(new_exprs));
     }
     Err(RuleApplicationError::RuleNotApplicable)
 }
